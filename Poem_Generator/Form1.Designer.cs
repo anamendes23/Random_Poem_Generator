11df0406522bdb175756b1f6cf1e997861360a1d
anamendes23 <anamendes23@gmail.com>
2018-05-29 09:52:38 -0700

    Added files and buttons work

---
+++ Poem_Generator/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Poem_Generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.BtnCatPoem = new System.Windows.Forms.Button();
+            this.BtnCupcakePoem = new System.Windows.Forms.Button();
+            this.BtnCoffeePoem = new System.Windows.Forms.Button();
+            this.BtnCustom = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(31, 72);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(322, 168);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(396, 72);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(526, 395);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // BtnCatPoem
+            // 
+            this.BtnCatPoem.Location = new System.Drawing.Point(31, 263);
+            this.BtnCatPoem.Name = "BtnCatPoem";
+            this.BtnCatPoem.Size = new System.Drawing.Size(322, 42);
+            this.BtnCatPoem.TabIndex = 2;
+            this.BtnCatPoem.Text = "Cat Haiku";
+            this.BtnCatPoem.UseVisualStyleBackColor = true;
+            this.BtnCatPoem.Click += new System.EventHandler(this.BtnCatPoem_Click);
+            // 
+            // BtnCupcakePoem
+            // 
+            this.BtnCupcakePoem.Location = new System.Drawing.Point(31, 318);
+            this.BtnCupcakePoem.Name = "BtnCupcakePoem";
+            this.BtnCupcakePoem.Size = new System.Drawing.Size(322, 42);
+            this.BtnCupcakePoem.TabIndex = 3;
+            this.BtnCupcakePoem.Text = "Cupcake Haiku";
+            this.BtnCupcakePoem.UseVisualStyleBackColor = true;
+            this.BtnCupcakePoem.Click += new System.EventHandler(this.BtnCupcakePoem_Click);
+            // 
+            // BtnCoffeePoem
+            // 
+            this.BtnCoffeePoem.Location = new System.Drawing.Point(31, 372);
+            this.BtnCoffeePoem.Name = "BtnCoffeePoem";
+            this.BtnCoffeePoem.Size = new System.Drawing.Size(322, 42);
+            this.BtnCoffeePoem.TabIndex = 4;
+            this.BtnCoffeePoem.Text = "Coffee Haiku";
+            this.BtnCoffeePoem.UseVisualStyleBackColor = true;
+            this.BtnCoffeePoem.Click += new System.EventHandler(this.BtnCoffeePoem_Click);
+            // 
+            // BtnCustom
+            // 
+            this.BtnCustom.Location = new System.Drawing.Point(31, 425);
+            this.BtnCustom.Name = "BtnCustom";
+            this.BtnCustom.Size = new System.Drawing.Size(322, 42);
+            this.BtnCustom.TabIndex = 5;
+            this.BtnCustom.Text = "Your Own Haiku";
+            this.BtnCustom.UseVisualStyleBackColor = true;
+            this.BtnCustom.Click += new System.EventHandler(this.BtnCustom_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(328, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Enter random words to create your own haiku";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(965, 492);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnCustom);
+            this.Controls.Add(this.BtnCoffeePoem);
+            this.Controls.Add(this.BtnCupcakePoem);
+            this.Controls.Add(this.BtnCatPoem);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button BtnCatPoem;
+        private System.Windows.Forms.Button BtnCupcakePoem;
+        private System.Windows.Forms.Button BtnCoffeePoem;
+        private System.Windows.Forms.Button BtnCustom;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Poem_Generator/Form1.cs
@@ -0,0 +1,145 @@
+﻿/* Ana Mendes
+ * anamendes23@gmail.com
+ * github.com/anamendes23
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Poem_Generator
+{
+    public partial class Form1 : Form
+    {
+        //list to hold random words
+        List<string> catIpsumList = new List<string>();
+        List<string> cupcakeIpsumList = new List<string>();
+        List<string> coffeeIpsumList = new List<string>();
+        List<string> customizedList = new List<string>();
+        //paths
+        string catPath = "catIpsum.txt";
+        string cupcakePath = "cupcakeIpsum.txt";
+        string coffeePath = "coffeeIpsum.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+            catIpsumList.Add(" aNahaA ");
+            TextFileReader(catIpsumList, catPath);
+            TextFileReader(cupcakeIpsumList, cupcakePath);
+            TextFileReader(coffeeIpsumList, coffeePath);
+        }
+
+        private void TextFileReader(List<string> list, string path)
+        {
+            FileStream fs = null;
+            StreamReader sr = null;
+
+            try
+            {
+                fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+                sr = new StreamReader(fs);
+
+                char[] separators = { ' ', ',', ':', '.', ';', '!', '?' };               
+
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    string[] words = line.Split(separators);
+                    for (int i = 0; i < words.Length; i++)
+                    {
+                        if(words[i]!="" || words[i] != " ")
+                            list.AddRange(words);
+                    }
+                }
+                for (int i = 0; i < list.Count; i++)
+                {
+                    if (list[i] == "I")
+                        continue;
+                    list[i].ToLower().Trim();
+                }
+            }
+            catch (ArgumentException ae)
+            {
+                MessageBox.Show(ae.Message);
+            }
+            catch (FileNotFoundException fne)
+            {
+                MessageBox.Show(fne.Message);
+            }
+            catch (IOException ioe)
+            {
+                MessageBox.Show(ioe.Message);
+            }
+            finally
+            {
+                if (fs != null) fs.Close();
+                if (sr != null) sr.Close();
+            }
+        }
+
+        private void GeneratePoem(List<string> list)
+        {
+            Random rand = new Random();
+            richTextBox2.Clear();
+
+            for (int i = 0; i < 9; i++)
+            {
+                if (i == 3 || i == 6)
+                    richTextBox2.AppendText("\n");
+                int nOfWords = rand.Next(3, 6);
+                for (int j = 0; j < nOfWords; j++)
+                {
+                    int randIndex = rand.Next(0, list.Count - 1);
+                    string word = list[randIndex];
+                    richTextBox2.AppendText(word + " ");
+                }
+                richTextBox2.AppendText("\n");
+            }
+        }
+
+        private void BtnCatPoem_Click(object sender, EventArgs e)
+        {
+            GeneratePoem(catIpsumList);
+        }
+
+        private void BtnCupcakePoem_Click(object sender, EventArgs e)
+        {
+            GeneratePoem(cupcakeIpsumList);
+        }
+
+        private void BtnCoffeePoem_Click(object sender, EventArgs e)
+        {
+            GeneratePoem(coffeeIpsumList);
+        }
+
+        private void BtnCustom_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string inputString = richTextBox1.Text;
+                char[] separators = { ' ', ',', ':', '.', ';', '!', '?' };
+                string[] words = inputString.Split(separators);
+
+                for (int i = 0; i < words.Length; i++)
+                {
+                    if (words[i] != "" || words[i] != " ")
+                        customizedList.AddRange(words);
+                }
+
+                GeneratePoem(customizedList);
+            }
+            catch(ArgumentException ae)
+            {
+                MessageBox.Show(ae.Message);
+            }
+        }
+    }
+}
